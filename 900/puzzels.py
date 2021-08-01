c55855d0d43a09bd987d02e6a51900d11622aebd
Pratyashshrivastava <86169534+Pratyashshrivastava@users.noreply.github.com>
2021-08-02 00:23:22 +0530

    Update puzzels.py

--- 900/puzzels.py
+++ 900/puzzels.py
@@ -6,9 +6,7 @@ k.sort()
 m=1000
 d=0
 for i in range(n[1]-n[0]+1):
-     l=k[i:n[0]-i+1]
-     if l==n[0]:
-        d=abs(l[n[0]-i+1]-l[i])
-     if d<m:
-         m=d
+   d=k[i+n[0]-1]-k[i]
+   if d<m:
+      m=d
 print(m)
